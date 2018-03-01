19c5d9a5ded398c59ee6b6757d297c3358dedc1d
Chinmi <elyesbarnat42@gmail.com>
2018-03-01 17:08:05 +0100

    Stateless Functionnal Components + data via props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class Header extends React.Component {
+/*class Header extends React.Component {
   render() {
     return (
       <header className="top">
@@ -18,7 +18,29 @@ class Header extends React.Component {
       </header>
     )
   }
-}
+*/
+
+// If my component is just getting in data via props and returns in JSX
+// I don't have to create a class
+// I can do that instead
+
+/*const Header = ({tagline}) => (*/ //this way too because props is an object
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+      {/*<span>{tagline}</span>*/}
+    </h3>
+  </header>
+)
 
 export default Header
 
