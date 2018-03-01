b7d1fc2636a8cd1032c770881cfa211a5983dae9
Chinmi <elyesbarnat42@gmail.com>
2018-03-01 16:19:03 +0100

    loading css

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,19 +1,15 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react"
 
 class StorePicker extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<h2>Please Enter A Store</h2>
-				{/* Her's how we can comment */}
-				<form className="store-selctor">
-					<input type="text" required placeholder="Store Name"/>
-					<button type="submit">Visit Store</button>
-				</form>
-			</React.Fragment>
-		)
-	}
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store →</button>
+      </form>
+    )
+  }
 }
 
 export default StorePicker
