3bec78a0c24af0d3159364a997ed5167b8d5d333
Chinmi <elyesbarnat42@gmail.com>
2018-03-02 14:48:42 +0100

    Upload Disturb me :  Understanding State (AddFish)

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,28 @@ import Inventory from './Inventory'
 import Order from './Order'
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  }
+
+  addFish = fish => {
+    // We must use the setState of React
+    const fishes = {...this.state.fishes}
+    fishes[`fish${Date.now()}`] = fish
+    this.setState({
+      fishes
+    })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
